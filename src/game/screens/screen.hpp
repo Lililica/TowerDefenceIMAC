a3dcd07e7e9e95df7b768e3014c22d8065f8cddb
Hellwyn <elvinkauffmann@gmail.com>
2024-05-24 09:28:27 +0200

    screen init

--- src/game/screens/screen.hpp
+++ src/game/screens/screen.hpp
@@ -1,13 +1,14 @@
 #pragma once
+#include <vector>
 
 enum state { MAIN_MENU, LEVEL, PAUSE_MENU, LEVEL_SELECTOR };
 
 struct screen{
 
-    state _state {LEVEL};
-
-
+    state _state {MAIN_MENU};
+    unsigned int levelId {};
     int nbrTileSide {20};
+    std::vector<Case> listCase {};
 
 
 };
