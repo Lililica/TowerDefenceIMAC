05972708d0e3ef90212a9c0e8085c488638b13ef
Hellwyn <elvinkauffmann@gmail.com>
2024-05-27 11:39:05 +0200

    Outils && fonction de collision pos+box + box+box

--- src/game/utilitaires/outil.cpp
+++ src/game/utilitaires/outil.cpp
@@ -1,3 +1,22 @@
 #include "./outil.hpp"
 
-bool collision_position_box(std::pair<double, double> pos, std::pair<double, double> pos_box, std::pair<double, double> size_box);
+bool collision_pos_box(std::pair<double, double> pos, std::pair<double, double> pos_box, std::pair<double, double> size_box) {
+    if(pos.first >= pos_box.first 
+        && pos.first <= pos_box.first+size_box.first
+        && pos.second >= pos_box.second 
+        && pos.second <= pos_box.second+size_box.second)
+        return true;
+    else return false;
+}
+bool collision_box_box(std::pair<double, double> pos_box1, std::pair<double, double> size_box1, std::pair<double, double> pos_box2, std::pair<double, double> size_box2) {
+    
+    if(
+        pos_box1.first+size_box1.first >= pos_box2.first && 
+            (pos_box1.second+size_box1.second <= pos_box2.second 
+            || pos_box1.second >= pos_box2.second+size_box2.second)
+        || pos_box1.first <= pos_box2.first+size_box2.first &&
+            (pos_box1.second+size_box1.second <= pos_box2.second 
+            || pos_box1.second >= pos_box2.second+size_box2.second) 
+    ) return true;
+    else return false;
+}
