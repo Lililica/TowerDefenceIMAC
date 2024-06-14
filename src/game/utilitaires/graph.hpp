be8117692acef1a931b273cba839c3ff2def448f
FannyRz <fanny.rz.celia@gmail.com>
2024-06-14 18:38:32 +0200

    copier-coller TD graph

--- src/game/utilitaires/graph.hpp
+++ src/game/utilitaires/graph.hpp
@@ -1,1 +1,43 @@
 #pragma once
+
+#pragma once
+
+#include <vector>
+#include <unordered_map>
+#include <utility>
+
+namespace Graph {
+    struct WeightedGraphEdge {
+        int to {};
+        float weight {1.0f};
+
+        // default ici permet de définit les opérateurs de comparaison membres à membres automatiquement
+        // Cela ne fonction qu'en C++20, si vous n'avez pas accès à cette version je vous donne les implémentations des opérateurs plus bas
+        bool operator==(WeightedGraphEdge const& other);
+        bool operator!=(WeightedGraphEdge const& other);
+    };
+
+    struct WeightedGraph {
+        // L'utilisation d'un tableau associatif permet d'avoir une complexité en O(1) pour l'ajout et la recherche d'un sommet.
+        // Cela permet de stocker les sommets dans un ordre quelconque (et pas avoir la contrainte d'avoir des identifiants (entiers) de sommets consécutifs lors de l'ajout de sommets).
+        // Cela permet également de pouvoir utiliser des identifiants de sommets de n'importe quel type (string, char, int, ...) et pas seulement des entiers.
+        std::unordered_map<int, std::vector<WeightedGraphEdge>> adjacency_list {};
+
+        void add_vertex(int const id);
+
+        void add_directed_edge(int const from, int const to, float const weight = 1.0f);
+        void add_undirected_edge(int const from, int const to, float const weight = 1.0f);
+        
+        // Même fonctionnement que pour WeightedGraphEdge
+        bool operator==(WeightedGraph const& other);
+        bool operator!=(WeightedGraph const& other);
+
+        void print_DFS(int const start);
+        std::vector<int> list_DFS(int const start);
+        void print_BFS(int const start);
+        std::vector<int> list_BFS(int const start);
+    };
+
+} // namespace
+
+std::vector<int> dijkstra_search(Graph::WeightedGraph & graph);
