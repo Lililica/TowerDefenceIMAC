de1c2672f9ba39614279dccc539f56291b08418c
Hellwyn <elvinkauffmann@gmail.com>
2024-06-14 15:47:30 +0200

    collision

--- src/game/utilitaires/outil.hpp
+++ src/game/utilitaires/outil.hpp
@@ -2,3 +2,4 @@
 #include <utility>
 
 bool collision_position_box(std::pair<double, double> pos, std::pair<double, double> pos_box, std::pair<double, double> size_box);
+bool collision_box_box(std::pair<double, double> pos_box1, std::pair<double, double> size_box1, std::pair<double, double> pos_box2, std::pair<double, double> size_box2);
