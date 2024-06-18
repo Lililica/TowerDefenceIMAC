7b8efc964f80c2d156fe1cb55c1462a2091212cb
Lililica <geogold.11@gmail.com>
2024-06-18 19:27:57 +0200

    ok y=test

--- src/game/draws/draw.cpp
+++ src/game/draws/draw.cpp
@@ -170,11 +170,11 @@ void App::draw_all_content(){
 
         if(myScreen.showCaseDispo){
             for(Case myCase : myScreen.listCase){
-                if(myCase._type == typeCase::DECOR){
+                if(myCase._type == typeCase::DECOR || myCase.occupied){
                     glPushMatrix();
                         glColor4f(1,0,0,0.9*abs(sin(currentTime*3)));
                         // glScalef(0.95,0.95,1);
-                        drawRect(  (myCase.pos.first), (myCase.pos.second), 2./myScreen.nbrTileSide, -2./myScreen.nbrTileSide);
+                        drawRect((myCase.pos.first), (myCase.pos.second), 2./myScreen.nbrTileSide, -2./myScreen.nbrTileSide);
                     glPopMatrix();    
                 }
             }
